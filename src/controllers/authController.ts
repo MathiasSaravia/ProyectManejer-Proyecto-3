9ee4ff7f85348f62d53f010d6996da5cdb3a426d
MathiasSaravia <maycolbeach@gmail.com>
2024-08-09 11:20:41 -0300

    add backend from register and login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,80 +1,110 @@
 import { Request, Response } from "express";
-import { errorResponse } from "../helpers";
+import { errorResponse, generateJWT, generateTokenRandom } from "../helpers";
+import createError from "http-errors";
+import User from "../models/User";
+import { compare } from "bcryptjs";
 
 
-export const register = async (req : Request,res : Response) => { 
-        try { 
 
-            return res.status(201).json({ 
-                ok : true, 
-                msg :'Usuario Registrado    ' 
-            }) 
-        } catch (error : unknown) {                 
+export const register = async (req: Request, res: Response) => {
+    try {
 
-            errorResponse(res , error , "REGISTER")
+        const { email, password, name } = req.body;
 
-        } 
+        if (!email || !password || !name) throw createError('Todos los campos son obligatorios');
+        if ([email, password, name].includes(''))throw createError('Todos los campos son obligatorios');
+
+        let user = await User.findOne({ email });
+        if (user) throw createError('El usuario ya existe');
+
+        user = new User(req.body);
+        user.token = generateTokenRandom();
+        const userStore = await user.save()
+
+        return res.status(201).json({
+            ok: true,
+            msg: 'Usuario Registrado',
+            data : userStore
+        })
+    } catch (error: unknown) {
+
+        errorResponse(res, error, "REGISTER")
+
     }
+}
 
-export const login = async (req : Request,res : Response) => { 
-        try { 
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) throw createError('Todos los campos son obligatorios');
 
-            return res.status(200).json({ 
-                ok : true, 
-                msg :'Usuario Logueado' 
-            }) 
-        } catch (error) { 
+        let user = await User.findOne({ email });
 
-            errorResponse(res , error , "LOGIN")
+        if (!user) throw createError('El usuario no existe');
+        if (!user.checked) throw createError('Tu cuenta no ha sido verificada');
+        if (!(await compare(password , user.password))) throw createError('La contraseña es incorrecta');
 
-        } 
+        return res.status(200).json({
+            ok: true,
+            msg: 'Usuario Logueado',
+            user: {
+                nombre: user.name,
+                email: user.email ,
+                token: generateJWT({ id: user._id })
+            }
+        })
+    } catch (error) {
+
+        errorResponse(res, error, "LOGIN")
+
     }
-    export const checked = async (req : Request,res : Response) => { 
-        try { 
+}
+export const checked = async (req: Request, res: Response) => {
+    try {
 
-            return res.status(201).json({ 
-                ok : true, 
-                msg :'Usuario Chequeado' 
-            }) 
-        } catch (error) { 
+        return res.status(201).json({
+            ok: true,
+            msg: 'Usuario Chequeado'
+        })
+    } catch (error) {
 
-            errorResponse(res , error , "CHECKED")
+        errorResponse(res, error, "CHECKED")
 
-        } 
     }
-export const sendToken = async (req : Request,res : Response) => { 
-        try { 
-            return res.status(200).json({ 
-                ok : true, 
-                msg :'Token enviado' 
-            }) 
-        } catch (error) { 
+}
+export const sendToken = async (req: Request, res: Response) => {
+    try {
+        return res.status(200).json({
+            ok: true,
+            msg: 'Token enviado'
+        })
+    } catch (error) {
 
-            errorResponse(res , error , "SEND-TOKEN")
+        errorResponse(res, error, "SEND-TOKEN")
 
-        } 
     }
-export const verifyToken = async (req : Request,res : Response) => { 
-        try { 
-            return res.status(200).json({ 
-                ok : true, 
-                msg :'Token verificado' 
-            }) 
-        } catch (error) { 
+}
+export const verifyToken = async (req: Request, res: Response) => {
+    try {
+        return res.status(200).json({
+            ok: true,
+            msg: 'Token verificado'
+        })
+    } catch (error) {
 
-            errorResponse(res , error , "VERIFY-TOKEN")
+        errorResponse(res, error, "VERIFY-TOKEN")
 
-        } 
     }
-export const changePassword = async (req : Request,res : Response) => { 
-        try { 
-            return res.status(200).json({ 
-                ok : true, 
-                msg :'Password actualizado' 
-            }) 
-        } catch (error) { 
+}
+export const changePassword = async (req: Request, res: Response) => {
+    try {
+        return res.status(200).json({
+            ok: true,
+            msg: 'Password actualizado'
+        })
+    } catch (error) {
 
-            errorResponse(res , error , "CHANGE-PASSWORD")
+        errorResponse(res, error, "CHANGE-PASSWORD")
 
-        } 
     }
+}
--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,39 +1,38 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const mongoose_1 = __importDefault(require("mongoose"));
-var projectSchema = new mongoose_1.default.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    dateExpire: {
-        type: Date,
-        default: Date.now(),
-    },
-    client: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    createdBy: {
-        type: mongoose_1.default.Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    collaborators: [
-        {
-            type: mongoose_1.default.Schema.Types.ObjectId,
-            ref: 'User',
-        }
-    ],
-}, {
-    timestamps: true,
-});
-module.exports = mongoose_1.default.model('Project', projectSchema);
+import mongoose from 'mongoose';  
+
+var projectSchema = new mongoose.Schema({ 
+    name:{ 
+        type:String, 
+        required:true, 
+        trim:true, 
+    }, 
+    description:{ 
+        type:String, 
+        required:true, 
+        trim:true, 
+    }, 
+    dateExpire:{ 
+        type:Date, 
+        default:Date.now(), 
+    }, 
+    client:{ 
+        type:String, 
+        required:true, 
+        trim:true, 
+    }, 
+    createdBy:{ 
+        type:mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+    }, 
+    collaborators :[ 
+
+        { 
+            type:mongoose.Schema.Types.ObjectId, 
+            ref: 'User', 
+        } 
+    ], 
+},{ 
+    timestamps : true, 
+}); 
+
+export = mongoose.model('Project', projectSchema);
